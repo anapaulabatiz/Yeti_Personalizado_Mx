28bb63b6e52b3af5785fb3d8e6de7afaa047e95a
Ana Paula Batiz <ap.batiz@gmail.com>
2023-05-18 18:55:21 -0600

    Bean activated

--- Yeti-Personalizado/src/main/java/mx/yetipersonalizado/YetiPersonalizadoApplication.java
+++ Yeti-Personalizado/src/main/java/mx/yetipersonalizado/YetiPersonalizadoApplication.java
@@ -14,15 +14,15 @@ public class YetiPersonalizadoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YetiPersonalizadoApplication.class, args);
 	} // main
-//	
-//	@Bean
-//	public FilterRegistrationBean<JwtFilter> jwtFilter() {
-//		FilterRegistrationBean<JwtFilter> registrationBean =
-//				new FilterRegistrationBean<>();
-//		registrationBean.setFilter(new JwtFilter());
-//		registrationBean.addUrlPatterns("/producto/*");
-//		registrationBean.addUrlPatterns("/usuario/*");
-//		return registrationBean;
-//	}
+	
+	@Bean
+	public FilterRegistrationBean<JwtFilter> jwtFilter() {
+		FilterRegistrationBean<JwtFilter> registrationBean =
+				new FilterRegistrationBean<>();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/producto/*");
+		registrationBean.addUrlPatterns("/usuario/*");
+		return registrationBean;
+	}
 
 } // class YetiPersonalizadoApplication
